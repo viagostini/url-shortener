ac85ffdf1d6db6f3f5628fe3fb42e6609389fc75
Vinicius Agostini <vinicius.agostini19@gmail.com>
2021-01-02 12:04:37 -0300

    feature: added docker support for development with docker-compose

--- url_shortener/backend/main.py
+++ url_shortener/backend/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-origins = ["http://localhost:8080"]
+origins = ["http://localhost:8080", "http://127.0.0.1:8080"]
 
 app.add_middleware(
     CORSMiddleware,
