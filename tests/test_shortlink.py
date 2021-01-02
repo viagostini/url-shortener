33e9505d398c31ad9e3984087949c9cbc79f6636
Vinicius Agostini <vinicius.agostini19@gmail.com>
2021-01-02 00:40:48 -0300

    adding basic tests for creating and retrieving links

---
+++ tests/test_shortlink.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+
+from url_shortener.backend.main import app, database
+
+client = TestClient(app)
+
+
+def test_create_shortlink():
+    response = client.post("/", json={"url": "testurl.com"})
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert "url" in response_json
+    assert "shortlink" in response_json
+    assert response_json["url"] == "testurl.com"
+
+    # not the most safe test
+    shortlink = response_json["shortlink"].split("/")[-1]
+    assert len(shortlink) == 7
+    assert shortlink.isalnum()
+    print(response_json["shortlink"])
+    # hacky check for insertion for now
+    assert database[shortlink] == "testurl.com"
+
+
+def test_get_full_url():
+    # hacky insert to database for now
+    database["1234567"] = "testurl.com"
+
+    response = client.get("/1234567")
+    assert response.status_code == 200
+    assert response.json() == {"url": "testurl.com"}
